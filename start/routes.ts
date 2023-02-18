5c75b45ec0c7f80efe3d9325d4016d70bf029773
Redomeire <redomeire2@gmail.com>
2023-02-18 11:03:40 +0700

    add: dummy route

--- start/routes.ts
+++ start/routes.ts
@@ -23,3 +23,21 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/', async () => {
   return { hello: 'world' }
 })
+
+Route.get('/users', () => {
+  const users = [
+    {
+      name: 'redo',
+      id: 1
+    },
+    {
+      name: 'redo',
+      id: 1
+    },
+    {
+      name: 'redo',
+      id: 1
+    }
+  ]
+  return { data: users }
+})
